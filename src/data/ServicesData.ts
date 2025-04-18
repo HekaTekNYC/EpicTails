1a3d91cb25cfaa40655d825ccdcee5358900577b
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-04-18 13:29:10 -0400

    Services section responsiveness
    service section responsiveness done, all new icons loaded in. Content wrapper taken out of main.

--- src/data/ServicesData.ts
+++ src/data/ServicesData.ts
@@ -1,4 +1,5 @@
 export interface ServiceData {
+  id: number
   icon: string
   iconAlt: string
   title: string
@@ -7,36 +8,42 @@ export interface ServiceData {
 
 export const servicesData: ServiceData[] = [
   {
+    id: 1,
     icon: "/icons/dog-leash.svg",
     iconAlt: "white dog on leash icon",
     title: "Dog Walking",
     desc: "Whether your pup needs a quick potty break or a long adventure, our walks are tailored to match their pace, personality, and needs.",
   },
   {
+    id: 2,
     icon: "/icons/pet-sitting.svg",
     iconAlt: "pet sitting icon",
     title: "Pet Sitting",
     desc: "Going out of town? We’ll keep your pets happy and comfortable right at home with personalized, reliable care while you're away.",
   },
   {
+    id: 3,
     icon: "/icons/critter.svg",
     iconAlt: "lizard icon",
     title: "Cat and Critter Care",
     desc: "From shy kitties to curious bunnies, we provide gentle, thoughtful care for all your feline and small animal companions.",
   },
   {
-    icon: "/icons/puppy.png",
+    id: 4,
+    icon: "/icons/puppy.svg",
     iconAlt: "puppy icon",
     title: "Puppy Visits",
     desc: "Perfect for new pups who need extra attention! We provide playtime, potty breaks, and lots of love during the day.",
   },
   {
-    icon: "/icons/overnight.png",
-    iconAlt: "overnight stay icon",
+    id: 5,
+    icon: "/icons/pet-bed.svg",
+    iconAlt: "pet bed icon",
     title: "Overnight Stays",
     desc: "Give your pet a sleepover buddy! We’ll stay the night to ensure they feel safe, secure, and well-cared for until morning.",
   },
   {
+    id: 6,
     icon: "/icons/pet-transport.svg",
     iconAlt: "white pet taxi icon",
     title: "Pet Transportation",
