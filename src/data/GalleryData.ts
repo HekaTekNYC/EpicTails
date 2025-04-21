2db56bc2ebe0e2af3ed811843f61f3b1fabfb424
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-04-21 16:53:59 -0400

    gallery edit and hero configuring
    hero edits - gallery altered for responsiveness. Gallery images put into a data file

---
+++ src/data/GalleryData.ts
@@ -0,0 +1,42 @@
+export const galleryImageData = [
+	{
+		webp: '/images/gallery-photos/shepherd-park.webp',
+		png: '/images/gallery-photos/shepherd-park.png',
+		alt: 'Shepherd in the park'
+	},
+	{
+		webp: '/images/gallery-photos/dog-in-street.webp',
+		png: '/images/gallery-photos/dog-in-street.png',
+		alt: 'Dog in the street'
+	},
+	{
+		webp: '/images/gallery-photos/black-catititude.webp',
+		png: '/images/gallery-photos/black-catititude.png',
+		alt: 'Black cat with attitude'
+	},
+	{
+		webp: '/images/gallery-photos/bengal-cat.webp',
+		png: '/images/gallery-photos/bengal-cat.png',
+		alt: 'Bengal cat'
+	},
+	{
+		webp: '/images/gallery-photos/dog-sitting-on-leash.webp',
+		png: '/images/gallery-photos/dog-sitting-on-leash.png',
+		alt: 'Dog sitting on a leash'
+	},
+	{
+		webp: '/images/gallery-photos/orange-menace.webp',
+		png: '/images/gallery-photos/orange-menace.png',
+		alt: 'Orange menace cat'
+	},
+	{
+		webp: '/images/gallery-photos/dog-in-snow.webp',
+		png: '/images/gallery-photos/dog-in-snow.png',
+		alt: 'Dog in the snow'
+	},
+	{
+		webp: '/images/gallery-photos/smug-kitty.webp',
+		png: '/images/gallery-photos/smug-kitty.png',
+		alt: 'Smug looking kitty'
+	}
+]
