f9590008370d62437f710fac4fe65e62337e54b2
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2025-04-24 15:31:59 -0400

    adjustments across site
    final adjustments and tweaks. disabled devtoolbar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import {defineConfig} from "astro/config"
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  //   output: "static", // Enables Static Site Generation (SSG)
-  trailingSlash: "always",
+	devToolbar: {
+		enabled: false
+	},
+	trailingSlash: 'always'
 })
